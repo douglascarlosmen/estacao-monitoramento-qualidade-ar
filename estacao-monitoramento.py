c77afff9b9cf59a58a829dbb5455068cdfddbeee
Douglas Men <contatodouglasmen@gmail.com>
2024-03-21 15:53:19 -0300

    Enviando dados para a api de coleta de dados

--- estacao-monitoramento.py
+++ estacao-monitoramento.py
@@ -1,15 +1,27 @@
 import random
+import json
+import requests
 from paho.mqtt import client as mqtt_client
 
 broker = 'localhost'
 port = 1883
 topic = "sensor/data"
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
+api_url = "http://localhost:5000/dados"
 
 # Função para lidar com a chegada de uma mensagem
 def on_message(client, userdata, message):
     print(f"Received `{message.payload.decode()}` from `{message.topic}` topic")
-    # Aqui, você pode adicionar o código para processar ou encaminhar os dados recebidos
+    data = json.loads(message.payload.decode())
+    enviar_para_api(data)
+
+def enviar_para_api(data):
+    # Envia os dados recebidos para a API
+    response = requests.post(api_url, json=data)
+    if response.status_code == 200:
+        print("Dados enviados com sucesso para a API.")
+    else:
+        print(f"Erro ao enviar dados para a API: {response.status_code}")
 
 def connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
